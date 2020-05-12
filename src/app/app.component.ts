0ab7202ec7f356afddef8436feb66838a7ad88c2
Kate Sky <kateskytech@outlook.com>
2020-05-12 10:46:05 -0500

    angular 9 started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import * as AOS from 'aos';
 import { Router } from '@angular/router';
 import { PodcastsService } from './services/podcasts/podcasts.service';
 import { Podcast } from './model/podcast';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -12,23 +13,13 @@ import { Podcast } from './model/podcast';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'countries';
-  podcast: Podcast;
+  title = 'podcast';
+  podcast$: Observable<Podcast> = this.podcastService.podcast$;
 
-  constructor(public countryService: CountryService, private podcastService: PodcastsService) { }
+  constructor( private podcastService: PodcastsService) { }
 
   ngOnInit() {
     AOS.init();
-    this.podcastService.getPodcast().subscribe((data: Podcast) => {
-      this.podcast = data;
-      console.log(this.podcast);
-    });
-
+    this.podcastService.getPodcast();
   }
-  changeMode() {
-    this.countryService.mode = !this.countryService.mode;
-    localStorage.setItem('mode', String(this.countryService.mode));
-  }
-
- 
 }
--- src/app/services/podcasts/podcasts.service.ts
+++ src/app/services/podcasts/podcasts.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Podcast } from 'src/app/model/podcast';
+import {  BehaviorSubject } from 'rxjs';
+import {  map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PodcastsService {
   podcast: Podcast;
+  podcast$: BehaviorSubject<Podcast> = new BehaviorSubject<Podcast>(null);
 
   constructor(private http: HttpClient) {
     // Make the HTTP request:
   }
 
   getPodcast() {
-    return this.http.get('/assets/data/podcasts.json');
+    this.http.get('/assets/data/podcasts.json').subscribe((data: Podcast) => {
+      this.podcast$.next(data);
+      console.log(this.podcast);
+    });
+
   }
 }
